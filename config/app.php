1f29dae661b706ee48801f28b4eaf28971571e29
francesco <moretto.francesco86@gmail.com>
2026-03-24 12:25:11 +0100

    Remove BASE_PATH definition logic from app.php

--- config/app.php
+++ config/app.php
@@ -1,4 +1,2 @@
 <?php
-$parts = explode('/', ltrim(str_replace('\\', '/', $_SERVER['SCRIPT_NAME'] ?? ''), '/'));
-define('BASE_PATH', '/' . ($parts[0] ?? ''));
-unset($parts);
+define('BASE_PATH', '');
